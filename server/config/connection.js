cada7d157eee8bd5dfeb6f0a4aa3db7e32c164a4
agabrie <agabriels@2u.com>
2022-10-28 13:18:26 +0200

    updated config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const db_port = process.env.DB_PORT||27017;
-const db_name = process.env.DB_NAME;
-const db_url = process.env.DB_URL;
-const connectionString = `mongodb://${db_url}:${db_port}/${db_name}`;
+const isProduction = process.env.NODE_ENV === 'production';
+const db_port = isProduction ? process.env.DB_PORT:(process.env.DB_DEV_PORT||27017);
+const db_name = isProduction ? process.env.DB_NAME:process.env.DB_DEV_NAME;
+const db_url = isProduction ? process.env.DB_URL:process.env.DB_DEV_URL;;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+
+var connectionString = `mongodb://${db_url}:${db_port}/feedback`;
+if(process.env.NODE_ENV === 'production'){
+    connectionString = `mongodb+srv://${db_user}:${db_password}@${db_name}/?retryWrites=true&w=majority`
+}
 
 mongoose.connect(connectionString,{
     useNewUrlParser:true,
